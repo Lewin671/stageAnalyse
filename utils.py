d2db27fd071c0ed1da64776ca513a09d1f38c9ba
Lewin671 <2596736318@qq.com>
2021-12-25 03:26:31 +0800

    chore: add options for recording screen

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ def cmd(command):
 
 
 def run_with_recording(func, file_name):
-    task = cmd('scrcpy --no-display --render-expired-frames --record  ' + file_name)
+    task = cmd('scrcpy --no-display --max-size 1024 --max-fps 120 --render-expired-frames --record ' + file_name)
     time.sleep(2)
     func()
     task.send_signal(signal.SIGINT)
