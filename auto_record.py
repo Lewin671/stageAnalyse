55efffb604937e39dd0e1a6700130c45bb4b2f47
Lewin671 <2596736318@qq.com>
2021-12-25 15:10:31 +0800

    chore: rename variable name and update ignore file

--- auto_record.py
+++ auto_record.py
@@ -16,7 +16,7 @@ def click(device):
     time.sleep(8)
 
 
-def record_task(file_name):
+def record_task(output_file_path: str):
     device = uiautomator2.connect(DEVICE_ID)
     logger.log("Device Info", device.info)
     device.app_start(PACKAGE_NAME, wait=True, stop=True)
@@ -27,10 +27,10 @@ def record_task(file_name):
     logger.log("ready to click button")
 
     # record clicking
-    raw_file_name = "./video/raw_" + file_name
-    file_name = "./video/" + file_name
+    raw_file_name = "./video/raw_" + output_file_path
+    output_file_path = "./video/" + output_file_path
     utils.run_with_recording(lambda: click(device), raw_file_name)
-    utils.cmd('ffmpeg -i ' + raw_file_name + " " + file_name).wait()
+    utils.cmd('ffmpeg -i ' + raw_file_name + " " + output_file_path).wait()
 
 
 if __name__ == '__main__':
