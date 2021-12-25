e3e4f32b8aa9ac760529dc49da6b101b5dd5efc6
Lewin671 <2596736318@qq.com>
2021-12-25 15:02:46 +0800

    refactor: generator and predictor use different threshold to make prediction more concise

--- cutter.py
+++ cutter.py
@@ -5,7 +5,7 @@ import config
 from hooks import HOOKS
 
 
-def cutting_video(file_name: str):
+def cutting_video(file_name: str, threshold: float = config.THRESHOLD):
     video = VideoObject(file_name)
     video.load_frames()
 
@@ -19,5 +19,5 @@ def cutting_video(file_name: str):
     res = cutter.cut(video, block=config.BLOCK_NUMBER)
 
     # classify sections by the its stability
-    stable, unstable = res.get_range(threshold=config.THRESHOLD, offset=config.OFFSET)
+    stable, unstable = res.get_range(threshold=threshold, offset=config.OFFSET)
     return stable, unstable, res
--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@ from cutter import cutting_video
 
 if __name__ == '__main__':
     file_name = config.TRAINING_VIDEO_PATH
-    stable, unstable, res = cutting_video(file_name)
+    stable, unstable, res = cutting_video(file_name, threshold=0.90)
 
     # 把分好类的稳定阶段的图片存本地
     res.pick_and_save(stable, 10, to_dir=config.TRAINING_SET_PATH, meaningful_name=True)
